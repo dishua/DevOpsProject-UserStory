d815b2f39b9d18a54f0325e2ab993a3f505dbdc5
Dmytro Shymanskyi <noob4ikua@gmail.com>
2026-08-11 20:21:58 +0300

    Added db folder with init.sql

---
+++ devops/Docker/config/db/init.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS userstory
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE userstory;
+
+CREATE TABLE IF NOT EXISTS projects (
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    description VARCHAR(255),
+    name        VARCHAR(255),
+    PRIMARY KEY (id)
+);
